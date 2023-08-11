d02a191bd849685427e90e60ac9ad80b03c63e53
mowahdan <m.wahdan@devsquads.com>
2023-08-11 18:57:46 +0300

    finish search by name or description and filter by category features

--- djangoEcommerce/item/views.py
+++ djangoEcommerce/item/views.py
@@ -1,14 +1,27 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 
 from .forms import NewItem, EditItem
-from .models import Item
+from .models import Item, Category
 
 def items(request):
+    query = request.GET.get('query', '')
     items = Item.objects.filter(is_sold=False)
+    categories = Category.objects.all()
+    category_id = request. GET.get('category', 0)
+    
+    if query:
+        items = items.filter(Q(name__icontains=query) | Q(description__icontains=query))
+        
+    if category_id:
+        items = items.filter(category_id = category_id)
     
     return render(request, 'item/items.html', {
         'items': items,
+        'query': query,
+        'categories': categories,
+        'category_id': int(category_id),
     })
 
 def details(request, item_id):
